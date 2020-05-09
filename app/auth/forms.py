21eefcd08604db8ebfe55ca30df5bcab56600b1c
Dynamo Denis <dmbugua66@gmail.com>
2020-05-09 17:23:53 +0300

    created registration forms

---
+++ app/auth/forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextField,SubmitField,PasswordField,ValidationError,BooleanField
+from wtforms.validators import DataRequired,Email,EqualTo,Length,InputRequired,ValidationError
+from ..models import RegularUser,Writter
+
+
+class RegistrationForm(FlaskForm):
+    username=StringField('Type Your Username:',validators=[DataRequired(),Length(min=2, max=30)])
+    email=StringField('Enter Your Email Address:', validators=[Email(),DataRequired()])
+    password=PasswordField('Enter Password:',validators=[DataRequired(),EqualTo('password_confirm',message='Passwords Must Match')])
+    password_confirm=PasswordField('Confirm Password',validators=[DataRequired()])
+    submit=SubmitField('Create Account')
+
+    def validate_username(self,data):
+        if RegularUser.query.filter_by(username=data.data).first() and Writter.query.filter_by(username=data.data).first():
+            raise ValidationError('This username is taken! Choose another username.')
+
+    def validate_email(self,data):
+        if RegularUser.query.filter_by(email=data.data).first() and Writter.query.filter_by(email=data.data).first():
+            raise ValidationError('This email is taken! Choose another email.')
+
+class LoginForm(FlaskForm):
+    email=StringField('Enter Your Email Address:', validators=[Email(),DataRequired()])
+    password=PasswordField('Enter Password:',validators=[InputRequired()])
+    remember=BooleanField('Remember me:')
+    submit=SubmitField('Login')
+ 
