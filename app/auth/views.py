fd40baf4f5752e584415037442af584d31d25030
Dynamo Denis <dmbugua66@gmail.com>
2020-05-09 22:22:25 +0300

    login view function

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,14 @@
 from . import auth
-from flask import render_template,redirect,request,url_for
+from flask import render_template,redirect,request,url_for,flash,abort
 from .forms import RegistrationForm,LoginForm
+from flask_login import login_user,login_required,logout_user
 
     
 @auth.route('/register', methods=['GET','POST'])
 def register():
     form=RegistrationForm()
     if form.validate_on_submit():
-        user=User(username=form.username.data,email=form.email.data,password=form.password.data)
+        user=User(username=form.username.data,email=form.email.data,category=form.category.data,password=form.password.data)
         db.session.add(user)
         db.session.commit()
 
@@ -15,3 +16,20 @@ def register():
         return redirect(url_for('auth.login'))
 
     return render_template('auth/register.html',form=form,title='Create Account')
+
+@auth.route('/login', methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user,form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+            flash('Login successful! Welcome to Blog Arena')
+
+    return render_template('auth/login.html', title='Login', form=form)
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
