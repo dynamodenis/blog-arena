10be4fffe63c061b3c67c664458b45eee00e8b95
Dynamo Denis <dmbugua66@gmail.com>
2020-05-09 10:10:48 +0300

    initial commit

---
+++ config.py
@@ -0,0 +1,17 @@
+class Config:
+    pass
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG=True
+
+class TestConfig(Config):
+    pass
+
+config_options={
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test':TestConfig
+}
